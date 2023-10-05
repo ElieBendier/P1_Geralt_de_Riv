c430cecf14db54dcfdea281888a65d70aeccd65c
nicolasalibert <contact@nicolasalibert.me>
2023-10-05 15:31:50 +0200

    Unified code for section menu

---
+++ section-bloc1.php
@@ -0,0 +1,45 @@
+<div class="bloc1">
+            <div class="button my-story">
+                <div class="button-logo">
+                    <a href="#my-story"></a>
+                </div>
+                <div class="button-title">
+                    <a href="#my-story">MY STORY</a>
+                </div>
+            </div>
+
+            <div class="button armory">
+                <div class="button-logo">
+                    <a href="#armory"></a>
+                </div>
+                <div class="button-title">
+                    <a href="#armory">Armory</a>
+                </div>
+            </div>
+
+            <div class="button skills">
+                <div class="button-logo">
+                    <a href="#skills"></a>
+                </div>
+                <div class="button-title">
+                    <a href="#skills">Skills</a>
+                </div>
+            </div>
+
+            <div class="button graveyard">
+                <div class="button-logo">
+                    <a href="#graveyard"></a>
+                </div>
+                <div class="button-title">
+                    <a href="#graveyard">Graveyard</a>
+                </div>
+            </div>
+            <div class="button back desktop">
+                <div class="button-logo ">
+                    <a href="#top"></a>
+                </div>
+                <div class="button-title">    
+                    <a href="#top">Home</a>
+                </div>
+            </div>
+</div>
